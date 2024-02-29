30232edc86210552150b8d4ca8b17fcd915060b9
manish <manishsogarwal2002@gmail.com>
2024-02-29 13:03:10 +0530

    new`

--- SQl_project.sql
+++ SQl_project.sql
@@ -93,10 +93,24 @@ VALUES
 ('4058','Boulder','USA','Colorado','91238'),
 ('4061','Fall River','USA','Massachusetts','90555');
 
+"Q1. Query all columns for all American cities in the CITY table with populations larger than 100000.
+The CountryCode for America is USA.
+The CITY table is described as follows:"
+
+--Ans.
+
 select * from SQl_table;
 select * from SQl_table
 where population >= 100000 and Country_code = 'USA'
 
 
+"Q2. Query the NAME field for all American cities in the CITY table with populations larger than 120000.
+The CountryCode for America is USA.
+The CITY table is described as follows:"
 
+--Ans.
+
+select Name
+from SQl_table
+where Country_Code = 'USA' and population >=120000;
 
